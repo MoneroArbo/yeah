7089d7337c7a940284b5b154e9472c44f8279dcd
Chronoment <rlocki69@gmail.com>
2024-05-24 22:56:47 -0400

    Delete components.js

--- components.js
+++
@@ -1,8 +0,0 @@
-const linkComponent = (title, icon_classes, url) => {
-  return `
-    <div>
-        <a href="${url}" target="_blank" class="links , hover">
-        ${title}</a>
-    </div>
-  `
-}
